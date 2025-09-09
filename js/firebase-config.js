618039c79fc525c7e6d91370395628e50b058597
panupongpormmin <panupongpormmin@gmail.com>
2025-09-10 06:45:15 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,13 @@
 {
-  "hosting": {
-    "public": ".",
-    "ignore": [
-      "firebase.json",
-      "**/.*",
-      "**/node_modules/**"
-    ]
-  }
+  "type": "service_account",
+  "project_id": "eratfarm-bd035",
+  "private_key_id": "1da3403cc53634cc3af8fe1886c7268d4d2d0c26",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCkIPmS06jVH0ys\neK+7Cpd46GZnuyaWiQgfzL6GPo4vsTtlVeHrXVAnnw0XmXxolTLUmI2kGTqhp/4O\nO1NpQ4a0Un9M2yQYgZnN3BAHm2yG8it2DWDFPshIt9yNFZaX19MirY3PaYkbm1xw\n/faGBtXGtmjAPFtq+YpZTDp5dSsEz7gLbjiiyB8f9MNFcCqN9sNkjQ2Yw1mnihm3\nPH1nMJOL/PUMyIrb+dZ4W2/mw5AdZNCWMVuFTEFNijzepQqj5GBCeJBXJGJgMmsb\nl4RwjlzFLNMdpxdaqK18t4Ecd4Ru0jdjQOWzL7raDve0/Ku6BB0nHw9pOVAVVYNM\nxt3AhjYLAgMBAAECggEADEhddQP7AAwKGGQlvokh4c3+j+e9EQnrPjn7zh8TU/Ve\n/Qvqzsraa0SnXatCWK9FsQxDmVK8BQoF5LnC2b9QbuE3nqBNuLnGMVY1KuSSQB0S\nbJC+u7Qhl/OOgU5qyS1BIzi6wplixdqgFeKjls8fI3JWL4C01LIdDOa7/4fpsr/c\nEuaRT00IlDhkVtD0qiZ0NzxSydrXQXOjpA3J5MNcrtb+lJu767B575p+BLPdYYsp\n7Mi1uPvEE6787exk+yjQsKsxL8Sbhp/w1FTX1coQ82lRlNgN1ktZoPu0rjWf6kjp\nQYf8/1pswriY4DnquT1Vt5jamKh462uSvkdCVlq0EQKBgQDUHCzsdhyXGCk3tLSn\nGTcMdgqilfsIjK+U6HycT7i6lHTXlTo5YTkzoNKn8LRp+k9PP9KEkzWmcKRwHZmf\nUG2f1sp6dR42Mj9CNvUYbV7VgAldmyDTyoT7gKLthq9uDge2awVbwtuHfcAtJebL\nw21mJDAR9uDbqaV0F1eQBN6DUQKBgQDGFyb6s6+H3FEJKp6s+SPp5GGVuYscMCEc\nPzzXbrpmxokcau8XBdzePOeQfeG7tlRMwG17BEeQAjBiJXsS0sG9jPY0AKyqYS7S\nwQBDbCH/MZpP110SMVBakEAFnSAccAqm13CsgF7XxfzqTcxA7+Dc6PY8PS3pAhf5\nm2AVCox0mwKBgC9X6p8M7ey7s9d/OiNh+WuKnW0U1Odfn4E5MzcghDcBE+Nap5hP\nHxlq+BoMG/jbqgAiWrbEtODlm456zONGbHGU27Thl1C5fm0lHyfIobY90dlW2sjr\nFyub1sH1ogqKjyYkVRnaMG3Zn590KkOo5y45PZGE7UOaZhe3bg/zRkdBAoGAPPG0\nkV8k+vSijeJp7Se9j+Eu0MTD8skv9qPSESnE+YfNeUvBAWFRYaJ1T/QatgjTiMWP\nKVDYv4qc3iVP5yumZa3vtXIbrVO29ypf7V5gW0YkhrnorYc0hJhqyg9caMNj047m\n4HuiMlp8hI2o8LWchfq6fzAHZu+49peCCRc0vU8CgYEAmvzrPfWJ9lUFrXYXFXaA\nKRXR9v2iduldXFHXtN6/wLeunbEjDbfwzMCBUcXG68DvUkr/vyw11vQzQJsvZO3Q\n6c7lDutc/wGxPkrtcbXBPw74xSlMgYZ+Q7aZdH172Xpsm8VFg4wLEmzmiT+xvqVD\nI7JXNGDv5G9ku/9a3NMZfPM=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@eratfarm-bd035.iam.gserviceaccount.com",
+  "client_id": "109559265675387580296",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40eratfarm-bd035.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
 }
